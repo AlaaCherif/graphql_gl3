edb1cff45c87fb6b4f9d569406f632bbb1938cd0
Alaa <alaaeddinecherif@insat.u-carthage.tn>
2022-04-27 15:22:43 +0200

    fixed addTodo

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { Query } from './resolvers/Query.js'
-import { Student } from './resolvers/Student.js'
-import { User } from './resolvers/User.js'
-import { Todo } from './resolvers/Todo.js'
+import { GraphQLServer, PubSub } from 'graphql-yoga';
+import { Query } from './resolvers/Query.js';
+import { Student } from './resolvers/Student.js';
+import { User } from './resolvers/User.js';
+import { Todo } from './resolvers/Todo.js';
+import { Mutation } from './resolvers/Mutation.js';
+import { Subscription } from './resolvers/Subscription.js';
+const pubsub = new PubSub();
 // ... or using "require()"
 // const { GraphQLServer } = require('graphql-yoga')
-const typeDefs = "schema/schema.graphql";
+const typeDefs = 'schema/schema.graphql';
 const resolvers = {
-    Query,
-    Student,
-    User,
-    Todo
+  Query,
+  Student,
+  User,
+  Todo,
+  Mutation,
+  Subscription,
 };
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
+server.start(() => console.log('Server is running on localhost:4000'));
